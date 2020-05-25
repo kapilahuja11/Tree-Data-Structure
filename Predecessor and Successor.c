9ab028d8a9740de696d9cb757e10a81b4471819a
kapil ahuja <kapil.ahuja.kapil11@gmail.com>
2020-05-25 23:53:07 +0530

    Create  Predecessor and Successor.c

---
+++ Predecessor and Successor.c
@@ -0,0 +1,20 @@
+struct Node{
+{
+	int key;
+	struct Node *left, *right;
+};
+
+// This function finds predecessor and successor of key in BST.
+// It sets pre and suc as predecessor and successor respectively
+void findPreSuc(Node* root, Node*& pre, Node*& suc, int key)
+{
+    if(root)
+    {
+        findPreSuc(root->left,pre,suc,key);
+        if(root->key < key)
+            pre = root;
+        if((root->key > key) && (suc == NULL))
+            suc = root;
+        findPreSuc(root->right,pre,suc,key);
+    }
+}
