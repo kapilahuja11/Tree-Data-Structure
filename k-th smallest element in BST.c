573492c78463375b1d91da21de6333c8e64a469b
kapil ahuja <kapil.ahuja.kapil11@gmail.com>
2020-05-25 23:45:22 +0530

    Create k-th smallest element in BST.c
    Input:
    2
    2 1 3
    2
    2 1 3
    5
    Output:
    2
    -1

---
+++ k-th smallest element in BST.c
@@ -0,0 +1,24 @@
+struct Node
+{
+    int data;
+    int lCount;
+ 
+    Node* left;
+    Node* right;
+};*/
+#include <vector>
+void inorder(Node *root,int k,vector<int> &vec)
+{
+    if(root==NULL)
+    return;
+    inorder(root->left,k,vec);
+    vec.push_back(root->data);
+    inorder(root->right,k,vec);
+}
+int KthSmallestElement(Node *root, int k)
+{
+    //add code here.
+    vector<int> vec;
+    inorder(root,k,vec);
+    return vec[k-1];
+}
