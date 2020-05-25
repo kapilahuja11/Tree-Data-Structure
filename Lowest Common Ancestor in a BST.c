6aba0b8861d02fd052756cf7dcff9d92222de27b
kapil ahuja <kapil.ahuja.kapil11@gmail.com>
2020-05-25 23:35:48 +0530

    Create  Lowest Common Ancestor in a BST.c

---
+++ Lowest Common Ancestor in a BST.c
@@ -0,0 +1,43 @@
+
+struct Node{
+    int data;
+    Node* right;
+    Node* left;
+    
+    Node(int x){
+        data = x;
+        right = NULL;
+        left = NULL;
+    }
+};
+/*You are required to complete
+this function*/
+Node* LCA(Node *root, int n1, int n2)
+{
+    if (root == NULL) return NULL; 
+  
+    if (root->data > n1 && root->data > n2) 
+        return LCA(root->left, n1, n2); 
+  
+    if (root->data < n1 && root->data < n2) 
+        return LCA(root->right, n1, n2); 
+  
+    return root; 
+}
+
+
+
+
+
+Node* LCA(Node *root, int n1, int n2)
+{
+    if (root == NULL) return NULL; 
+  
+    if (root->data > n1 && root->data > n2) 
+        return LCA(root->left, n1, n2); 
+  
+    if (root->data < n1 && root->data < n2) 
+        return LCA(root->right, n1, n2); 
+  
+    return root; 
+}
