c57ae164c6da88bdb3b4c79ba204169581d2402d
kapil ahuja <kapil.ahuja.kapil11@gmail.com>
2020-06-09 13:37:37 +0530

    Create Level order traversal Line by Line.cpp
    TC: o(n)
    AS: o(w) where w is width of a Tree

---
+++ Level order traversal Line by Line.cpp
@@ -0,0 +1,25 @@
+void levelOrder(Node* root)
+{
+  //Your code here
+  if(root==NULL)
+  return;
+  queue<Node*> q;
+  q.push(root);
+  while(q.empty()==false)
+  {
+      int count=q.size();
+      for(int i=0;i<count;i++)
+      {
+          Node *curr=q.front();
+          q.pop();
+          cout<<(curr->data)<<" ";
+          if(curr->left!=NULL)
+          q.push(curr->left);
+          if(curr->right!=NULL)
+          q.push(curr->right);
+      }
+      cout<<"$"<<" ";
+  }
+}
+
+
