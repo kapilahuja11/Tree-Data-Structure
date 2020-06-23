b25a58c3c20b6570b768b3853d0c8aadaaab9f09
kapil ahuja <kapil.ahuja.kapil11@gmail.com>
2020-06-23 19:00:53 +0530

    Create Binary Tree to DLL.cpp

---
+++ Binary Tree to DLL.cpp
@@ -0,0 +1,21 @@
+void bToDLL(Node *root, Node **head)
+{
+    // your code here
+    if(root==NULL)
+    return;
+    static Node *prev =NULL;
+    bToDLL(root->left,head);
+    if(*head==NULL)
+    {
+     prev=NULL;    
+    *head=root;
+    }
+    else
+    {
+        root->left=prev;
+        prev->right=root;
+        
+    }
+    prev=root;
+    bToDLL(root->right,head);
+}
