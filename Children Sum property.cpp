ba6a46cd59b373868508519131c1664bb7a0cb58
kapil ahuja <kapil.ahuja.kapil11@gmail.com>
2020-06-23 16:06:34 +0530

    Create Children Sum property.cpp

---
+++ Children Sum property.cpp
@@ -0,0 +1,14 @@
+bool issum(Node *root)
+{
+  if(root==NULL)
+    return true;
+  if(root->left==NULL && root->right==NULL)
+    return true;
+  int sum=0;
+  if(root->left!=NULL)
+      sum+=root->left->key;
+  if(root->right != NULL)
+      sum+=root->right->key;
+  return (root->key == sum && issum(root->left) && issum(root->right);
+  
+}
